0918d3431b67d021e3130b11e2f9a9903a14eb6e
alnishad <a.shadrin@innopolis.university>
2021-02-22 19:24:30 +0300

    add deployment script

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 var request = require("supertest");
 var app = require("../index.js");describe("GET /", function() {
  it("respond with SNE MAKES ME CRY AGAIN!!! ON AND ON!!!", function(done) { //navigate to root and check the the response is "SNE MAKES ME CRY!!!"
- request(app).get("/").expect("SNE MAKES ME CRY AGAIN!!! ON AND ON!!!", done);
+ request(app).get("/").expect("SNE MAKES ME CRY!!!", done);
  });
 });
