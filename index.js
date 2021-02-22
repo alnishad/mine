d186e6ab1d555b3ef708d01e77f2a5fe875285f1
alnishad <a.shadrin@innopolis.university>
2021-02-22 19:05:53 +0300

    add deployment script

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 //importing node framework
 var express = require("express");
  
-var app = express();//Respond with "SNE MAKES ME CRY!!!" for requests that hit our root "/"
+var app = express();//Respond with "SNE MAKES ME CRY AGAIN!!!" for requests that hit our root "/"
 app.get("/", function (req, res) {
- res.send("SNE MAKES ME CRY");
+ res.send("SNE MAKES ME CRY AGAIN!!!");
 });//listen to port 12345 by default
 app.listen(process.env.PORT || 12345);
  
--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 var request = require("supertest");
 var app = require("../index.js");describe("GET /", function() {
- it("respond with SNE MAKES ME CRY!!!", function(done) { //navigate to root and check the the response is "SNE MAKES ME CRY!!!"
- request(app).get("/").expect("SNE MAKES ME CRY!!!", done);
+ it("respond with SNE MAKES ME CRY AGAIN!!!", function(done) { //navigate to root and check the the response is "SNE MAKES ME CRY!!!"
+ request(app).get("/").expect("SNE MAKES ME CRY AGAIN!!!", done);
  });
 });
