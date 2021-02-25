13303c14e871bfdccb4b55d6f7ea8f02b1840492
alnishad <a.shadrin@innopolis.university>
2021-02-25 15:12:34 +0300

    final

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 var request = require("supertest");
 var app = require("../index.js");describe("GET /", function() {
  it("respond with hello world", function(done) { //navigate to root and check the the response is "hello world"
- request(app).get("/").expect("SNE MAKES ME CRY AGAIN!!! OVER AND OVER!!!", done);
+ request(app).get("/").expect("SNE MAKES ME CRY AGAIN!!! OVER AND OVER!!! FINAL!!!", done);
  });
 });
