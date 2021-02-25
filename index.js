dd3744c5013ece72b7c95b9b7e9f2ee1fb287282
alnishad <a.shadrin@innopolis.university>
2021-02-25 17:12:53 +0300

    DEMO

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require("express");
  
 var app = express();//Respond with "SNE MAKES ME CRY AGAIN!!! ON ADN ON!!!" for requests that hit our root "/"
 app.get("/", function (req, res) {
- res.send("SNE MAKES ME CRY AGAIN!!! OVER AND OVER!!! FINAL ONE!!! ON AND ON!!!");
+ res.send("SNE MAKES ME CRY!!!");
 });//listen to port 12345 by default
 app.listen(process.env.PORT || 12345);
  
--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 var request = require("supertest");
 var app = require("../index.js");describe("GET /", function() {
  it("respond with hello world", function(done) { //navigate to root and check the the response is "hello world"
- request(app).get("/").expect("SNE MAKES ME CRY AGAIN!!! OVER AND OVER!!! FINAL ONE!!! ON AND ON!!!", done);
+ request(app).get("/").expect("SNE MAKES ME CRY AGAIN!!!", done);
  });
 });
